a2f1271de14de15ef7af17313be52512b74a5635
Angelo Silvestri <54776549+ANGIKILLER@users.noreply.github.com>
2024-02-02 15:50:33 +0100

    Update cmdtemplateClass.py

--- cmdtemplateClass.py
+++ cmdtemplateClass.py
@@ -4,36 +4,55 @@ from threading import Thread
 
 class Terminal:
     def __init__(self):
+        # Ext Vars
+        self.example = "This is an Example"
+        self.example_list = [1, 2, "Example"]
+
+        # Sys vars
         self.running = True
         self.user = getpass.getuser()
+        self.startbar = f"Terminal Template by 👾@angikiller - 2024"
         self.pointer = f"\n{self.user} --> "
         self.cin : str
         self.args : list
         self.command : str
+
+
+    def _clear(self):
+        os.system("clear")
+        print(f"{self.startbar}")
         
+
     def run(self):
-        print("Terminal Template - 2022")
+        self._clear()
         while self.running: self._input()
+
 
     def _input(self) -> None:
         self.cin = input(self.pointer)
-        self.args = self.cin.split(" ")[1:]
-        self.cont = self.cin.split(" ", 1)[1:]
-        self.command = self.cin.split(" ")[0]
+        self.command = self.cin.split(" ", 1)[0].lower()
+        try:
+            self.full_args = self.cin.split(" ", 1)[1]
+            self.args = self.cin.split(" ")[1:]
+        except: 
+            self.args, self.full_args = None, None
         self._process()
 
+
     def _process(self):
-        if self.command == "help":
+        if self.command in ["help"]:
             print("Non ci sono troppi comandi per adesso")
 
-        elif self.command == "os":
+        elif self.command in ["os"]:
            print(platform.system())
 
-        elif self.command == "":
+        elif self.command in ["test"]:
             None
 
         else:
             print("Nessun comando oppure non valido")
+
+
 
 
 t = Terminal()
